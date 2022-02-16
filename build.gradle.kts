4a941ca80e705c864b7b63e8dd6ec8c4da9e8acb
myyxl <22593897+myyxl@users.noreply.github.com>
2022-02-16 09:41:36 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "alphanet"
+version = "0.0.1"
+application {
+    mainClass.set("alphanet.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+}
+
+dependencies {
+    implementation("com.pi4j:pi4j-core:2.1.1")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-websockets-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ src/main/kotlin/alphanet/Application.kt
@@ -0,0 +1,12 @@
+package alphanet
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import alphanet.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSockets()
+    }.start(wait = true)
+}
